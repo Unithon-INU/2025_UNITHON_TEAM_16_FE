0bcfce8b3dbc9d59867c541f0a86dcee6c138f0f
GHY25 <hygga.1119@gmail.com>
2025-06-27 23:34:50 +0900

    2025/06/27

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes/Router';
 import Header from './components/Header';
+import './App.css'; // padding-top 정의했다면 import
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Router />
+      <div className="main-content">
+        <Router />
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
-import SearchBar from './SearchBar'; // 추가
 
 function Header() {
   return (
-    <header className="header">
-      <div className="header__left">
-        <Link to="/" className="header__logo">
-          🌐 HIRE_U
-        </Link>
+    <header className="navigation">
+      <div className="nav-container">
+        <div className="logo-container">
+          <Link to="/" className="logo">
+            HIRE U
+          </Link>
+          <div className="logo-icon"></div>
+        </div>
+
+        <nav className="nav-links">
+          <Link to="/notice">Notice</Link>
+          <Link to="/community">Community</Link>
+          <Link to="/law">Law</Link>
+          <Link to="/chat">Chat</Link>
+          <Link to="/setting">Setting</Link>
+        </nav>
+
+        <div className="nav-actions">
+          <div className="search-icon"></div>
+          <Link to="/login" className="sign-in-link">
+            Sign in
+          </Link>
+          <Link to="/signup" className="sign-up-btn">
+            Sign up
+          </Link>
+        </div>
       </div>
-      <nav className="header__nav">
-        <Link to="/">홈</Link>
-        <Link to="/board">게시판</Link>
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
-      </nav>
-      <SearchBar /> {/* 여기에 컴포넌트 삽입 */}
     </header>
   );
 }
