5c6cf0492a4712f0b93638fa3b15becd55fc4f01
wani1209 <chaewan120@naver.com>
2025-06-28 02:25:48 +0900

    git commit -m "feat: Replace LanguageSelector with Landingpage component
    - Delete LanguageSelector.js and create Landingpage.js
    - Add Figma-based design with earth globe background
    - Create Landingpage.css with accurate positioning
    - Add earth-globe.png asset
    - Update App.js imports and routing"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import LanguageSelector from './pages/LanguageSelector';
+import Landingpage from './pages/Landingpage';
 import Home from './pages/Home';
 import ChatList from './pages/ChatList';
 import ChatRoom from './pages/ChatRoom';
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LanguageSelector />} />
+        <Route path="/" element={<Landingpage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
